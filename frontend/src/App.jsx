449ed5df9073945577a4a7af2bb36660f9c05a48
Talha Ali <growwithtalha2@gmail.com>
2025-04-24 17:57:48 +0500

    added middleware

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,25 @@ import AdminPanel from "./components/AdminPanel";
 import "./index.css";
 import Register from './pages/Register';
 import Login from './pages/Login';
-
+import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/" element={<Quiz />} />
-                <Route path="/admin" element={<AdminPanel />} />
+                <Route path="/register" element={
+                    <PublicRoute><Register /></PublicRoute>
+                } />
+                <Route path="/login" element={
+                    <PublicRoute><Login /></PublicRoute>
+                } />
+                <Route path="/" element={
+                    <ProtectedRoute><Quiz /></ProtectedRoute>
+                } />
+                <Route path="/admin" element={
+                    <ProtectedRoute><AdminPanel /></ProtectedRoute>
+                } />
                 <Route path="*" element={<Quiz />} />
             </Routes>
         </Router>
